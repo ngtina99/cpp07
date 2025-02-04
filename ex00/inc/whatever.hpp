ffeb39bf757e8963af7fc820464c7d2a5c724e86
Tina <nguyen.valentina.99@gmail.com>
2025-02-05 00:16:43 +0100

    added

--- ex00/inc/whatever.hpp
+++ ex00/inc/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 01:13:43 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/02/03 01:16:56 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/02/05 00:14:42 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,25 @@
 
 #include <iostream>
 
+template <typename T> 
+void	swap(T &valueA, T &valueB) {
+
+	T valueTemp ;
+	valueTemp = valueA;
+	valueA = valueB;
+	valueB = valueTemp;
+
+}
+
+template <typename T>
+T min(T value1, T value2) {
+	return(value1 < value2 ? value1 : value2);
+}
+
+template <typename T> 
+T max(T value1, T value2) {
+	return(value1 > value2 ? value1 : value2);
+}
 
 
 #endif
