2d90bb5c378294b79815e82e2f47046c06d41b81
Tina <nguyen.valentina.99@gmail.com>
2025-02-14 16:42:38 +0100

    added

--- ex00/inc/whatever.hpp
+++ ex00/inc/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 01:13:43 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/02/05 00:14:42 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/02/14 16:38:16 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 template <typename T> 
 void	swap(T &valueA, T &valueB) {
 
-	T valueTemp ;
+	T valueTemp;
 	valueTemp = valueA;
 	valueA = valueB;
 	valueB = valueTemp;
@@ -34,6 +34,5 @@ template <typename T>
 T max(T value1, T value2) {
 	return(value1 > value2 ? value1 : value2);
 }
-
 
 #endif
